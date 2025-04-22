342a37745cbd4eaf712b16ab6e53820cbed506d5
elsonsoares <elson.soares@outlook.com>
2025-04-22 02:09:10 -0300

    Corrigindo caminho do arquivo pkl mais uma vez

--- 01_modelo_basico/servicos/predicao.py
+++ 01_modelo_basico/servicos/predicao.py
@@ -7,10 +7,10 @@ from datetime import datetime
 import os
 
 # Carregar modelo treinado uma única vez.
-# modelo = joblib.load('01_modelo_basico/modelo_treinado.pkl')
-caminho_base = os.path.dirname(os.path.abspath(__file__))
-caminho_modelo = os.path.join(caminho_base, '..', '01_modelo_basico', 'modelo_treinado.pkl')
-modelo = joblib.load(os.path.abspath(caminho_modelo))
+modelo = joblib.load('01_modelo_basico/modelo_treinado.pkl')
+#caminho_base = os.path.dirname(os.path.abspath(__file__))
+#caminho_modelo = os.path.join(caminho_base, '..', '01_modelo_basico', 'modelo_treinado.pkl')
+#modelo = joblib.load(os.path.abspath(caminho_modelo))
 
 
 # Função para extrair o caminho da decisão
